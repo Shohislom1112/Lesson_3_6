305993522bea18395197d4c159e6c3c2de29f1c6
Shohislom1112 <ravshanovshohislom11@gmail.com>
2023-12-28 13:29:04 +0500

    asdasd

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { logo } from "../../assets";
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header1">
+        <div className="container">
+          <div className="header">
+            <div className="ong">
+              <img src={logo} alt="" />
+
+              <a href="#" className="menu">
+                Продукция
+              </a>
+              <a href="#" className="menu">
+                Сертификаты
+              </a>
+              <a href="#" className="menu">
+                Наша команда
+              </a>
+              <a href="#" className="menu">
+                О нас
+              </a>
+              <a href="#" className="menu">
+                Новости
+              </a>
+              <a href="#" className="menu">
+                Вакансии
+              </a>
+              <a href="#" className="menu">
+                Контакты
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
